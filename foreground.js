4126f23d656ed812b45f6ea47b2ea5de0df745dc
HoSoo Lee <hosoo.lee@arithnea.de>
2020-12-12 22:41:50 +0100

    *18:14 crateed button

--- foreground.js
+++ foreground.js
@@ -1,1 +1,12 @@
-document.querySelector("#hplogo img").classList.add("spinspinspin");
+// document.querySelector("#hplogo img").classList.add("spinspinspin");
+
+const first = document.createElement("button");
+first.innerText = "SET DATA";
+first.id = "first";
+
+const second = document.createElement("button");
+second.innerText = "Shout out to backend";
+second.id = "second";
+
+document.querySelector("body").appendChild(first);
+document.querySelector("body").append(second);
