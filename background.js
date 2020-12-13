f1f9575b1a71288699aca7dd5040eb290e782a09
HoSoo Lee <hosoo.lee@arithnea.de>
2020-12-13 22:42:16 +0100

    *end update

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
+let active_tab_id = 0;
+
 chrome.tabs.onActivated.addListener((tab) => {
   chrome.tabs.get(tab.tabId, (current_tab_info) => {
+    active_tab_id = tab.tabId;
     if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
       chrome.tabs.insertCSS(null, { file: "./mystyles.css" });
       chrome.tabs.executeScript(null, { file: "./foreground.js" }, () =>
@@ -8,3 +11,13 @@ chrome.tabs.onActivated.addListener((tab) => {
     }
   });
 });
+
+chrome.runtime.onMessage.addListener(request, sender, sendResponse) => {
+  if (request.message ==='yo check the astorage') {
+    chrome.tabs.sendMessage(tabid, {message: 'yo i got your message'})
+    sendResponse({message: 'yo I got your message'});
+    chrome.storage.local.get("password", value => {
+      console.log(value)
+    });
+  }
+}
--- foreground.js
+++ foreground.js
@@ -15,3 +15,12 @@ first.addEventListener("click", () => {
   chrome.storage.local.set({ password: "123" });
   console.log("I SET DATA");
 });
+
+second.addEventListener("click", () => {
+  chrome.runtime.sendMessage({ message: "yo check the storage" }(res));
+  console.log("I SENT THE MESSAGE");
+});
+
+chrome.runtime.onMessage.addEventListener((request, sender, sendResponse) => {
+  console.log(request.message);
+});
